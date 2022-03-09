99cae611fc83d81358bc8370f64e4896a07cfa85
jsan <ahsan.wtc@gmail.com>
2022-03-09 15:23:35 +0500

    disabling warning

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ function App({ web3, accounts, contracts }) {
     }
     init();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   
   if(user.selectedToken === undefined) {
